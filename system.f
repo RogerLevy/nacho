2e01b9f16389803bd14e12eaf46c2915785b1d9a
Unknown <roger.levy@gmail.com>
2018-03-22 11:57:33 -0400

    Remove "DIR" stuff and put in bucket

--- system.f
+++ system.f
@@ -13,13 +13,6 @@ action idle ( -- )
 action walk ( -- )
 action animate ( val speed -- )
 
-: dirtable  create does> swap cells + @ ;
-
-dirtable dir>ang  \ east , southeast , south , southwest , west , northwest , north , northeast ,
-    0 , 45 , 90 , 135 , 180 , 225 , 270 , 315 ,
-
-dirtable dir>rad  \ east , southeast , south , southwest , west , northwest , north , northeast ,
-    0 , 45 >rad , 90 >rad , 135 >rad , 180 >rad , 225 >rad , 270 >rad , 315 >rad ,
 
 : halt  vx 0v ;
 : xaxis   0  <left> kstate if  drop -1 then  <right> kstate if  drop 1  then ;
