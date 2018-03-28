a1008c622f20685629039952bd6d54753538fc86
Roger Levy <roger.levy@gmail.com>
2018-03-27 23:35:10 -0400

    dev.f slash fix for linux (temporary solution)

--- dev.f
+++ dev.f
@@ -4,10 +4,14 @@ $000100 include ramen/ramen.f
    >r begin  2dup r@ scan
       ?dup while  2swap 2drop  #1 /string
    repeat  r> 2drop ;
+[defined] linux [if]
+: -name ( a n -- a n )  2dup  [char] / ending  nip -  #1 -  0 max ;
+[else]
 : -name ( a n -- a n )  2dup  [char] \ ending  nip -  #1 -  0 max ;
+[then]
 : strconst  create string, does> count ;
 
-including -name strconst prjpath
+including 2dup type -name strconst prjpath
 : rld  prjpath " /main.f" strjoin included ;
 
 [defined] source-id [if]  source-id close-file  drop [then]
